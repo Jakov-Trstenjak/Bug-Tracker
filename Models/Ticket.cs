8b018f36e23a43ee2ef7f9310addd78eb0dc8d3b
Jakov Trstenjak <jakov.trstenjak@gmail.com>
2020-04-08 19:55:16 +0200

    Changed ticket controller. Added simple form.

---
+++ Models/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bug_Tracker.Models
+{
+    public class Ticket
+    {
+
+        
+        public int TicketID { get; set; }
+
+        [Display(Name ="Title")]
+        public string TicketTitle { get; set; }
+
+        public  string UserID { get; set; }
+        
+
+        [Display(Name = "Describe occured problem")]
+        public string Description { get; set; }
+
+        public string Date { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string BugID { get; set; }
+
+        public string Priority { get; set; }
+
+
+    }
+}
