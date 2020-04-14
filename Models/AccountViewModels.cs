f880d283792a77cee3bd8403fbf6cb81d8a239a8
Jakov Trstenjak <jakov.trstenjak@gmail.com>
2020-04-14 18:17:45 +0200

    Added registration page.

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace Bug_Tracker.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string Username { get; set; }
     }
 
     public class ResetPasswordViewModel
