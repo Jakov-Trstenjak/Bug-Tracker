55c4882248fc8ae13836dac1ca806a7d9c66a7e8
Jakov Trstenjak <jakov.trstenjak@gmail.com>
2020-05-06 20:11:39 +0200

    A

--- Models/Bug.cs
+++ Models/Bug.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading;
 
 namespace Bug_Tracker.Models
 {
     public class Bug
     {
-        public int BugID { get; set; }
+        static int nextID;
+
+        public  int BugID { get; private set; }
     
         public string Description { get; set; }
 
-        public int PriorityID { get; set; }
+        public  int PriorityID { get; set; }
+
+
+        // auto-generate BugID
+        Bug()
+        {
+            BugID = Interlocked.Increment(ref nextID);
+        }
     
     }
 }
