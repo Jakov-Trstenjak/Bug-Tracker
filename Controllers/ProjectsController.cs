01a66b8f0d682071c7d0dca681da12556cf0805b
Jakov Trstenjak <jakov.trstenjak@gmail.com>
2020-05-11 02:08:41 +0200

    Projects display

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using Bug_Tracker.Models;
+using Bug_Tracker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bug_Tracker.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        // GET: Projects
+        public ActionResult ViewProjects(string username)
+        {
+            List<ProjectViewModel> allProjects = DapperORM.getProjects();
+
+            return View(allProjects);
+        }
+    }
+}
---
+++ ViewModels/ProjectViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bug_Tracker.Models;
+
+namespace Bug_Tracker.ViewModels
+{
+    public class ProjectViewModel
+    {
+        public int projektID { get; set; }
+        public string opisProjekta { get; set; }
+        public string nazivProjekta { get; set; }
+        public string datumPokretanja { get; set; }
+
+        public int brojListica { get; set; }
+
+        public ProjectViewModel()
+        {
+            brojListica = 0;
+        }
+
+    }
+}
